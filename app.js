8cecbde00504f1051f2e6bdb5dbc28da4edaab68
Michael Frank <mfrankwork@gmail.com>
2025-02-02 13:59:04 -0500

    Updates

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime';
+import { GetObjectCommand, PutObjectCommand, S3Client, S3ServiceException } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { fromBase64 } from '@smithy/util-base64';
 
-// Create Amazon Bedrock Runtime client
+// Create Amazon Bedrock Runtime and S3 clients
 const REGION = 'us-east-1';
 const config = { region: REGION };
-const client = new BedrockRuntimeClient(config);
+const bedrockClient = new BedrockRuntimeClient(config);
+const s3Client = new S3Client(config);
+
+const BUCKET_NAME = 'case-consulting-mgmt-genbot-images';
+const PRESIGNED_URL_EXPIRES_IN_DAYS = 7;
+
+const MAX_SEED = 1048576;
 
 /**
- * Invokes Amazon Bedrock model.
+ * Invokes Amazon Bedrock model to convert prompt text to image.
  *
  * @param {string} prompt - The input text prompt for the model.
  * @param {string} [modelId] - The ID of the model to use. Defaults to "amazon.nova-canvas-v1:0".
- * @returns {string} Base64-encoded image
+ * @returns {Uint8Array} Image bytes
  */
 export const invokeModel = async (prompt, modelId = 'amazon.nova-canvas-v1:0') => {
   try {
     // Prepare the payload
     console.log(`Preparing payload for text prompt: ${prompt}`);
+    const seed = Math.random() * MAX_SEED; // default is 12
     const payload = {
       taskType: 'TEXT_IMAGE',
       textToImageParams: {
@@ -28,7 +37,7 @@ export const invokeModel = async (prompt, modelId = 'amazon.nova-canvas-v1:0') =
         width: 320, // default is 1024. Minimum is 320.
         quality: 'standard', // default is 'standard'
         cfgScale: 8.0, // default is 6.5
-        seed: 42 // default is 12
+        seed
       }
     };
 
@@ -41,7 +50,7 @@ export const invokeModel = async (prompt, modelId = 'amazon.nova-canvas-v1:0') =
       body: JSON.stringify(payload)
     };
     const command = new InvokeModelCommand(params);
-    const response = await client.send(command);
+    const response = await bedrockClient.send(command);
 
     // Decode response and return image
     const responseBody = JSON.parse(new TextDecoder().decode(response.body));
@@ -55,8 +64,46 @@ export const invokeModel = async (prompt, modelId = 'amazon.nova-canvas-v1:0') =
 
     console.log(`Successfully generated image with Amazon Nova Canvas model ${modelId}`);
     console.log(`Image generated successfully. Image size: ${imageBuffer.length} bytes`);
-    return base64Image;
+    return imageBuffer;
   } catch (error) {
+    throw error;
+  }
+};
+
+/**
+ * Saves image to bucket and returns presigned URL to get image file from bucket.
+ *
+ * @param {Uint8Array} imageBuffer - The image bytes.
+ * @param {string} requestId - The identifier of the Lambda invocation request.
+ * @returns {string} Presigned URL to get image file from bucket
+ */
+export const saveImageToBucket = async (imageBuffer, requestId) => {
+  try {
+    // Write image file to bucket
+    const imageFileName = `${requestId}.png`;
+    console.log(`Saving ${imageFileName} image to ${BUCKET_NAME} bucket`);
+    const putCommand = new PutObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: imageFileName,
+      Body: imageBuffer,
+      ContentType: 'image/png'
+    });
+    const response = await s3Client.send(putCommand);
+    console.log(response);
+
+    // Return presigned URL to get image file from bucket
+    const getCommand = new GetObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: imageFileName
+    });
+    const expiresInSeconds = PRESIGNED_URL_EXPIRES_IN_DAYS * 24 * 60 * 60; // # days * 24 hours/day * 60 mins/hour * 60 secs/hour
+    console.log(`Generating presigned URL that expires in ${expiresInSeconds} seconds`);
+    const url = await getSignedUrl(s3Client, getCommand, { expiresIn: expiresInSeconds });
+    return url;
+  } catch (error) {
+    if (error instanceof S3ServiceException) {
+      console.error(`Error from S3 while uploading object to ${BUCKET_NAME}.  ${error.name}: ${error.message}`);
+    }
     throw error;
   }
 };
@@ -79,18 +126,18 @@ export const invokeModel = async (prompt, modelId = 'amazon.nova-canvas-v1:0') =
 export const handler = async (event, context) => {
   let response;
 
+  const requestId = context.awsRequestId;
   const body = JSON.parse(event.body);
   const command = body.command;
   const companyId = body.creator.company.id;
 
   if (companyId == process.env.companyId) {
     try {
-      const base64Image = await invokeModel(command);
+      const imageBuffer = await invokeModel(command);
+      const url = await saveImageToBucket(imageBuffer, requestId);
       response = {
         statusCode: 200,
-        contentType: 'image/png',
-        isBase64Encoded: true,
-        body: base64Image
+        body: url
       };
     } catch (error) {
       console.error('Error generating image:', error);
